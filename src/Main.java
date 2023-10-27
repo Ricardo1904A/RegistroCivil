78117b130f1a9031b243a07d08fc51c36312222e
Matriculacion <ricardoalexander1904@gmail.com>
2023-10-26 21:15:40 -0500

    Codigo Final

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-       RegistroCivil gestor = new RegistroCivil(2);// creamos la instancia de la clase RegistroCivil en este caso 2 por mayor facilidad
-        gestor.menu();//llamamos al metodo del menu para ejecutar el programa
+       RegistroCivil ejecutar = new RegistroCivil(2);// creamos la instancia de la clase RegistroCivil en este caso 2 por mayor facilidad
+        ejecutar.menu();//llamamos al metodo del menu para ejecutar el programa
     }
 }
